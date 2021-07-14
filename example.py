94a9b43fd14424f4f9e19cf68cc7fada29fd8ad7
markus-kari <87301752+markus-kari@users.noreply.github.com>
2021-07-14 15:02:45 +0200

    Serve a file through a web server

--- example.py
+++ example.py
@@ -1,1 +1,15 @@
-print('hello world')
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse
+from starlette.routing import Route, Mount
+from starlette.staticfiles import StaticFiles
+
+
+def homepage(request):
+    return PlainTextResponse('This is the main page.')
+
+routes = [
+    Route('/', homepage),
+    Mount('/static', StaticFiles(directory="python-with-oleg")),
+]
+
+app = Starlette(debug=True, routes=routes)
